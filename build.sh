78765a7a8a845808832bbf01c130933c3a6399de
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-04-02 12:04:00 +0200

    fixup

--- build.sh
+++
@@ -1,6 +0,0 @@
-if test -f bob/fn.sh ; then
-	echo ivan
-	echo again
-	echo master10
-fi
-test
--- hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-printf "Hello world\n" > hello.txt
-
-# Local Variables:
-# compile-command: "sh hello.sh"
-# End:
-#
-# vi:et:sw=4:ts=4:
