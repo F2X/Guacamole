b49eb383264b39bf9573988d4731c8d03d09d614
Francois-Xavier Gentilhomme <fx.gentilhomme@myscript.com>
2015-04-03 14:19:05 +0200

    Script update

--- release.sh
+++ release.sh
@@ -3,12 +3,33 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH="$(dirname "$SCRIPT")"
 
+UPDATE_TYPE='major'
+
 DESCRIBE=`git describe --tags --always`
 
 # increment the build number (ie 115 to 116)
 MAJOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[1]}'`
 MINOR=`echo $DESCRIBE | awk '{split($0,a,"."); print a[2]}'`
 PATCH=`echo $DESCRIBE | awk '{split($0,a,"."); print a[3]}'`
+
+echo "Current version: ${DESCRIBE}"
+
+case ${UPDATE_TYPE} in
+    major ) MAJOR=$((MAJOR+1))
+        ;;
+    minor) MINOR=$((MINOR+1))
+        ;;
+    patch ) PATCH=$((PATCH+1))
+        ;;
+    * ) echo "Wrong update-type (major|minor|patch)." && exit -1
+esac
+UPDATE_VERSION=${MAJOR}.${MINOR}.${PATCH}
+echo "Update version: ${UPDATE_VERSION}"
+
+git tag ${UPDATE_VERSION}
+git checkout -b ${UPDATE_VERSION}-release
+git rebase -i ${DESCRIBE}
+
 #
 #if [[ "${DESCRIBE}" =~ ^[A-Fa-f0-9]+$ ]]; then
 #    VERSION="0.0.0"
@@ -27,6 +48,4 @@ PATCH=`echo $DESCRIBE | awk '{split($0,a,"."); print a[3]}'`
 #
 #echo ${VERSION}+build.${BUILD}.${PATCH}
 
-echo "major: " + ${MAJOR}
-echo "minor: " + ${MINOR}
-echo "patch: " + ${PATCH}
+exit 0
